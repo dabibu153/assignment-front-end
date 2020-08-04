633dd6ac16052f7e3cd2b7f36cb72b94aff6514d
dabibu153 <68466028+dabibu153@users.noreply.github.com>
2020-08-04 22:13:11 +0530

    firdt commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-function App() {
+export default function App() {
+  const [r, setR] = useState(Math.random());
+  const [FirstName, setFName] = useState("saurav" + r);
+  const [LastName, setLName] = useState("sharma" + r);
+  const [email, setEmail] = useState(`saurav.sh315@gmail.com`);
+  const [phone, setPhone] = useState("1234567890");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form
+        action="http://localhost:5000"
+        method="POST"
+        enctype="multipart/form-data"
+      >
+        <label>First Name</label>
+        <input
+          type="text"
+          name="FirstName"
+          value={FirstName}
+          onChange={(e) => setFName(e.target.value)}
+        />
+        <br></br>
+        <label>Last Name</label>
+        <input
+          type="text"
+          name="LastName"
+          value={LastName}
+          onChange={(e) => setLName(e.target.value)}
+        ></input>
+        <br></br>
+        <label>email</label>
+        <input
+          type="email"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        ></input>
+        <br></br>
+        <label>phone</label>
+        <input
+          type="number"
+          name="phone"
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
+        ></input>
+        <br></br>
+        <label>image</label>
+        <input type="file" name="img" accept="image/*"></input>
+        <button type="submit">submit</button>
+      </form>
     </div>
   );
 }
-
-export default App;
